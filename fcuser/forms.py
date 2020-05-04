8ae3a03491ea6106f2ecca941ee282d240184c6e
wowgod9504 <mike1124@naver.com>
2020-05-05 00:12:15 +0900

    게시판 - 작성 폼,리스트,상세뷰

--- fcuser/forms.py
+++ fcuser/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Fcuser
 from django.contrib.auth.hashers import check_password
+from django.core.exceptions import ValidationError
 
 class LoginForm(forms.Form) :
     username = forms.CharField(
@@ -21,7 +22,14 @@ class LoginForm(forms.Form) :
 
         if username and password :
             fcuser = Fcuser.objects.get(username=username)
-            if not check_password(password, fcuser.password) :
-                self.add_error('password','비밀번호를 틀렸습니다.')
+            # user = Fcuser.objects.all() # 쿼리문 전체 테이블 데이터 할당 [<{key :value},{key :value}>,<{}>
+            # fcuser = user.get(username=username)
+            #  # username 가진 row 한줄 가져옴 <{key :value},{key :value}>
+            # # if fcuser not in user : # dict = dict 으로 비교 해야됌 -? 쿼리문을 dict으로 나타내야됨
+            # #     self.add_error('username','등록아이디가 아닙니다.')
+            # if not fcuser :
+            #     raise ValidationError('등록된 아이디가 없습니다.')
+            if not check_password(password,fcuser.password) :
+                self.add_error('password','비밀번호가 틀렸습니다.')
             else:
                 self.user_id = fcuser.id
